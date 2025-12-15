f4cb5cfe9fb7e6e58ea225c9b6af2bf9fee9722c
swapnil12348 <80088954+swapnil12348@users.noreply.github.com>
2025-12-15 10:59:31 +0530

    email reminder

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,7 @@
 import { Inngest } from "inngest";
 import User from "../models/User.js";
+import Connection from "../models/Connection.js";
+import sendEmail from "../configs/nodeMailer.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "pingup-app" });
@@ -98,6 +100,67 @@ const syncUserDeletion = inngest.createFunction(
         }
     }
 );
+//inngest fucntion to send reminder when a new connection request is added
+
+const sendNewConnectionRequestReminder = inngest.createFunction(
+    {id:"send-new-connection-request-reminder"},
+    {event:"app/connection-request"},
+    async ({event, step}) => {
+        const {connectionId}=event.data;
+        await step.run('send-connection-request-email', async () => {
+            const connection = await Connection.findById(connectionId).populate('from_user_id to_user_id');
+            const subject = `New Connection Request`;
+            const body = `<div style="font-family: Arial, sans-serif; padding: 20px;">
+            <h2>HI ${connection.to_user_id.full_name},</h2>
+            <p>You have a new connection request from ${connection.from_user_id.full_name} - @${connection.from_user_id.username}</p>
+            <p>Click <a href="${process.env.FRONTEND_URL}/connections" style="color:#10b981">here</a> to accept or reject the request</p>
+            <br/>
+            <p>Thanks,<br/>PingUp - Stay Connected</p>
+            </div>`;
+
+            await sendEmail ({
+                to: connection.to_user_id.email,
+                subject,
+                body
+            })
+            
+        })
+
+        const in24Hours=new Date(Date.now()+24*60*60*1000)
+        await step.sleepUntil("wait-for-24-hours", in24Hours);
+        await step.run('send-connection-request-reminder', async () => {
+            const connection = await Connection.findById(connectionId).populate('from_user_id to_user_id');
+
+            if (connection.status==="accepted") {
+                return{message:"Connection already accepted"};
+
+            }
+
+
+            const subject = `New Connection Request`;
+            const body = `<div style="font-family: Arial, sans-serif; padding: 20px;">
+            <h2>HI ${connection.to_user_id.full_name},</h2>
+            <p>You have a new connection request from ${connection.from_user_id.full_name} - @${connection.from_user_id.username}</p>
+            <p>Click <a href="${process.env.FRONTEND_URL}/connections" style="color:#10b981">here</a> to accept or reject the request</p>
+            <br/>
+            <p>Thanks,<br/>PingUp - Stay Connected</p>
+            </div>`;
+
+            await sendEmail ({
+                to: connection.to_user_id.email,
+                subject,
+                body
+            })
+
+            return {message:"Reminder Email Sent"};
+
+            
+            
+        })
+        
+    }
+    
+)
 
 // Export all Inngest functions
-export const functions = [syncUserCreation, syncUserUpdation, syncUserDeletion];
+export const functions = [syncUserCreation, syncUserUpdation, syncUserDeletion, sendNewConnectionRequestReminder];
--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ import 'dotenv/config';
 import { serve } from 'inngest/express';
 import connectDB from './configs/db.js';
 import { inngest, functions } from './inngest/index.js';
+import { clerkMiddleware } from '@clerk/express'
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 
@@ -12,10 +14,13 @@ await connectDB();
 
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware());
+
 
 app.get('/', (req, res) => res.send('Server is running...'));
-
 app.use('/api/inngest', serve({ client: inngest, functions }));
+app.use('/api/user', userRouter)
+
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
